414c7e6eea5f5663b1d82cac31659aa912635bfb
LoweKTH <62517085+LoweKTH@users.noreply.github.com>
2025-11-19 17:37:18 +0100

    Refactor create function in reportService

--- backend/src/services/reportService.ts
+++ backend/src/services/reportService.ts
@@ -2,7 +2,7 @@ import { createReportSchema } from '../validation/reportSchemas.js';
 import * as repo from '../repositories/reportRepository.js';
 
 export async function create(payload: unknown){
-    const parsed = createReportSchema.parse(payload); //kastar vid ogiltig payload
+    const parsed = createReportSchema.parse(payload); 
     return repo.insert(parsed.report_date, parsed.comment);
 }
 
